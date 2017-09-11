13d01059a584018c4afba862e774aba9cb4b1c05
wanggene <onedna@gmail.com>
2017-09-11 01:14:13 -0400

    update db and models

--- data_django/pubmed/models.py
+++ data_django/pubmed/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Abstract(models.Model):
 	pmid = models.CharField(max_length=50)
 	title = models.CharField(max_length=500)
-	author = models.CharField(max_length=250)
-	journal = models.CharField(max_length=250)
+	keywords = models.CharField(max_length=250, null=True, blank=True)
 	abstract = models.CharField(max_length=1000, null=True, blank=True)
-
+	link = models.CharField(max_length=500, null=True, blank=True)
+	
 	def __str__(self):
 		return self.pmid + ': ' + self.title
 
